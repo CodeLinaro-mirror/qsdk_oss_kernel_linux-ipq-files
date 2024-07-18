CodeLinaro-mirror/qsdk_oss_kernel_linux-ipq-5.4

net: enabling default skb recycler at run time.
The default value of  skb_recycler_enable is set to 0 for QSDK_512 profiles while
1 for QSDK_premium/enterprise profiles. The value 0 indicates skb recycler
is disabled while 1 indicates the recycler is enabled.
The  skb_recycler_enable is exposed through proc file system.
After boot-mode, we can enable/disable skb recycler explicitly only for
Alder/Miami QSDK_512 profiles or QSDK_premium/enterprise profiles.

Change-Id: I02eebf0e734c39605945f07a5a8316d52293aef5
Signed-off-by: Swati Singh <quic_swasing@quicinc.com>

net/core/skbuff_recycle.c
@@ -33,6 +33,8 @@ static struct global_recycler glob_recycler;
 static int skb_recycle_spare_max_skbs = SKB_RECYCLE_SPARE_MAX_SKBS;
 #endif
 
+static int skb_recycling_enable = 1;
+
 #ifdef CONFIG_DEBUG_MEM_USAGE
 #define mem_debug_update_skb(skb) \
 do { \
@@ -60,6 +62,11 @@ inline struct sk_buff *skb_recycler_alloc(struct net_device *dev,
 	struct sk_buff_head *h;
 	struct sk_buff *skb = NULL;
 	struct sk_buff *ln = NULL;
+
+	/* Allocate the recycled skbs if the skb_recycling_enable */
+	if (unlikely(!skb_recycling_enable)) {
+		return NULL;
+	}
 
 	if (unlikely(length > SKB_RECYCLE_SIZE))
 		return NULL;
@@ -167,6 +174,12 @@ inline bool skb_recycler_consume(struct sk_buff *skb)
 	unsigned long flags;
 	struct sk_buff_head *h;
 	struct sk_buff *ln = NULL;
+
+	/* Consume the skbs if the skb_recycling_enable */
+	if (unlikely(!skb_recycling_enable)) {
+		return false;
+	}
+
 	/* Can we recycle this skb?  If not, simply return that we cannot */
 	if (unlikely(!consume_skb_can_recycle(skb, SKB_RECYCLE_MIN_SIZE,
 					      SKB_RECYCLE_MAX_SIZE)))
@@ -276,7 +289,9 @@ inline bool skb_recycler_consume(struct sk_buff *skb)
 inline bool skb_recycler_consume_list_fast(struct sk_buff_head *skb_list)
 {
 	struct sk_buff *skb = NULL, *next = NULL;
-
+	if (unlikely(!skb_recycling_enable)) {
+		return false;
+	}
 	skb_queue_walk_safe(skb_list, skb, next) {
 		if (skb) {
 			__skb_unlink(skb, skb_list);
@@ -291,6 +306,11 @@ inline bool skb_recycler_consume_list_fast(struct sk_buff_head *skb_list)
 {
 	unsigned long flags;
 	struct sk_buff_head *h;
+
+	/* Allocate the recycled skbs if the skb_recycling_enable */
+	if (unlikely(!skb_recycling_enable)) {
+		return false;
+	}
 
 	h = &get_cpu_var(recycle_list);
 	local_irq_save(flags);
@@ -564,6 +584,53 @@ static const struct file_operations proc_skb_max_spare_skbs_fops = {
 };
 #endif /* CONFIG_SKB_RECYCLER_MULTI_CPU */
 
+/* procfs: skb_recycler_enable
+ * By default, recycler is disabled for QSDK_512 profile.
+ * Can be enabled for alder/miami QSDK_512 profile.
+ */
+static int proc_skb_recycler_enable_show(struct seq_file *seq, void *v)
+{
+        seq_printf(seq, "%d\n", skb_recycling_enable);
+        return 0;
+}
+
+static int proc_skb_recycle_enable_open(struct inode *inode, struct file *file)
+{
+        return single_open(file,
+                           proc_skb_recycler_enable_show,
+                           PDE_DATA(inode));
+}
+
+static ssize_t
+proc_skb_recycle_enable_write(struct file *file,
+                              const char __user *buf,
+                              size_t count,
+                              loff_t *ppos)
+{
+        int ret;
+        int enable;
+        char buffer[13];
+
+        memset(buffer, 0, sizeof(buffer));
+        if (count > sizeof(buffer) - 1)
+                count = sizeof(buffer) - 1;
+        if (copy_from_user(buffer, buf, count) != 0)
+                return -EFAULT;
+        ret = kstrtoint(strstrip(buffer), 10, &enable);
+        if (ret == 0 && enable >= 0)
+                skb_recycling_enable = enable;
+
+        return count;
+}
+
+static const struct file_operations proc_skb_recycle_enable_fops = {
+        .owner   = THIS_MODULE,
+        .open    = proc_skb_recycle_enable_open,
+        .read    = seq_read,
+        .write   = proc_skb_recycle_enable_write,
+        .release = single_release,
+};
+
 static void skb_recycler_init_procfs(void)
 {
 	proc_net_skbrecycler = proc_mkdir("skb_recycler", init_net.proc_net);
@@ -597,6 +664,12 @@ static void skb_recycler_init_procfs(void)
 			 &proc_skb_max_spare_skbs_fops))
 		pr_err("cannot create proc net skb_recycle max_spare_skbs\n");
 #endif
+
+	if (!proc_create("skb_recycler_enable",
+                         S_IRUGO | S_IWUGO,
+                         proc_net_skbrecycler,
+                         &proc_skb_recycle_enable_fops))
+                pr_err("cannot create proc net skb_recycle enable\n");
 }
 
 void __init skb_recycler_init(void)

CodeLinaro-mirror/qsdk_oss_kernel_linux-ipq-files

net/core/skbuff_recycle.c
@@ -32,6 +32,7 @@ static struct global_recycler glob_recycler;
 static int skb_recycle_spare_max_skbs = SKB_RECYCLE_SPARE_MAX_SKBS;
 #endif
 
+static int skb_recycling_enable = 1;
 /**
  * skb_recycler_clear_flags - Clear skb flags
  * @skb: skb pointer
@@ -58,6 +59,11 @@ inline struct sk_buff *skb_recycler_alloc(struct net_device *dev,
 	struct sk_buff_head *h;
 	struct sk_buff *skb = NULL;
 	struct sk_buff *ln = NULL;
+
+	/* Allocate the recycled skbs if the skb_recycling_enable */
+	if (unlikely(!skb_recycling_enable)) {
+		return NULL;
+	}
 
 	if (unlikely(length > SKB_RECYCLE_SIZE))
 		return NULL;
@@ -164,6 +170,12 @@ inline bool skb_recycler_consume(struct sk_buff *skb)
 	unsigned long flags;
 	struct sk_buff_head *h;
 	struct sk_buff *ln = NULL;
+
+	/* Consume the skbs if the skb_recycling_enable */
+	if (unlikely(!skb_recycling_enable)) {
+		return false;
+	}
+
 	/* Can we recycle this skb?  If not, simply return that we cannot */
 	if (unlikely(!consume_skb_can_recycle(skb, SKB_RECYCLE_MIN_SIZE,
 					      SKB_RECYCLE_MAX_SIZE)))
@@ -270,7 +282,9 @@ inline bool skb_recycler_consume(struct sk_buff *skb)
 inline bool skb_recycler_consume_list_fast(struct sk_buff_head *skb_list)
 {
 	struct sk_buff *skb = NULL, *next = NULL;
-
+	if (unlikely(!skb_recycling_enable)) {
+		return false;
+	}
 	skb_queue_walk_safe(skb_list, skb, next) {
 		if (skb) {
 			__skb_unlink(skb, skb_list);
@@ -285,6 +299,11 @@ inline bool skb_recycler_consume_list_fast(struct sk_buff_head *skb_list)
 {
 	unsigned long flags;
 	struct sk_buff_head *h;
+
+	/* Allocate the recycled skbs if the skb_recycling_enable */
+	if (unlikely(!skb_recycling_enable)) {
+		return false;
+	}
 
 	h = &get_cpu_var(recycle_list);
 	local_irq_save(flags);
@@ -570,6 +589,52 @@ static const struct proc_ops proc_skb_max_spare_skbs_fops = {
 };
 #endif /* CONFIG_SKB_RECYCLER_MULTI_CPU */
 
+/* procfs: skb_recycler_enable
+ * By default, recycler is disabled for QSDK_512 profile.
+ * Can be enabled for alder/miami QSDK_512 profile.
+ */
+static int proc_skb_recycler_enable_show(struct seq_file *seq, void *v)
+{
+        seq_printf(seq, "%d\n", skb_recycling_enable);
+        return 0;
+}
+
+static int proc_skb_recycle_enable_open(struct inode *inode, struct file *file)
+{
+        return single_open(file,
+                           proc_skb_recycler_enable_show,
+                           pde_data(inode));
+}
+
+static ssize_t
+proc_skb_recycle_enable_write(struct file *file,
+                              const char __user *buf,
+                              size_t count,
+                              loff_t *ppos)
+{
+        int ret;
+        int enable;
+        char buffer[13];
+
+        memset(buffer, 0, sizeof(buffer));
+        if (count > sizeof(buffer) - 1)
+                count = sizeof(buffer) - 1;
+        if (copy_from_user(buffer, buf, count) != 0)
+                return -EFAULT;
+        ret = kstrtoint(strstrip(buffer), 10, &enable);
+        if (ret == 0 && enable >= 0)
+                skb_recycling_enable = enable;
+
+        return count;
+}
+
+static const struct proc_ops proc_skb_recycle_enable_fops = {
+        .proc_open    = proc_skb_recycle_enable_open,
+        .proc_read    = seq_read,
+        .proc_write   = proc_skb_recycle_enable_write,
+        .proc_release = single_release,
+};
+
 static void skb_recycler_init_procfs(void)
 {
 	proc_net_skbrecycler = proc_mkdir("skb_recycler", init_net.proc_net);
@@ -603,6 +668,12 @@ static void skb_recycler_init_procfs(void)
 			 &proc_skb_max_spare_skbs_fops))
 		pr_err("cannot create proc net skb_recycle max_spare_skbs\n");
 #endif
+
+	if (!proc_create("skb_recycler_enable",
+                         S_IRUGO | S_IWUGO,
+                         proc_net_skbrecycler,
+                         &proc_skb_recycle_enable_fops))
+                pr_err("cannot create proc net skb_recycle enable\n");
 }
 
 void __init skb_recycler_init(void)