594d85d35d0e07f930e8226150d3ff3af21b56b3
Sridharan S N <quic_sridsn@quicinc.com>
2025-02-07 17:28:26 +0530

    remoteproc: qcom_q6v5: Do ioremap once during dump copy
    Fixes:1103cd1 "drivers: remoteproc: q6v5_mpd: do ioremap directly for coredump"
    Change-Id: Ia3579043d995f00b299ecddf007cf22a61097a2e
    Signed-off-by: Sridharan S N <quic_sridsn@quicinc.com>

--- drivers/remoteproc/qcom_q6v5_mpd.c
+++ drivers/remoteproc/qcom_q6v5_mpd.c
@@ -1131,17 +1131,26 @@ static void q6_wcss_copy_segment(struct rproc *rproc,
 {
 	struct q6_wcss *wcss = rproc->priv;
 	struct device *dev = wcss->dev;
-	void *ptr;
 
-	ptr = devm_ioremap_wc(dev, segment->da, segment->size);
-	if (!ptr) {
+	if (!segment->io_ptr) {
+		segment->io_ptr = devm_ioremap_wc(dev, segment->da, segment->size);
+		dev_dbg(dev, "ioremap region io_ptr:0x%px da:0x%pad size:%zx\n",
+			segment->io_ptr, &segment->da, segment->size);
+	}
+
+	if (!segment->io_ptr) {
 		dev_err(dev, "Failed to ioremap segment %pad size %zx\n",
 			&segment->da, segment->size);
 		return;
 	}
 
-	memcpy(dest, ptr + offset, size);
-	devm_iounmap(dev, ptr);
+	memcpy(dest, segment->io_ptr + offset, size);
+	if (offset + size >= segment->size) {
+		dev_dbg(dev, "iounmap region io_ptr:0x%px da:0x%pad size:%zx\n",
+			segment->io_ptr, &segment->da, segment->size);
+		devm_iounmap(dev, segment->io_ptr);
+		segment->io_ptr = NULL;
+	}
 }
 
 static int get_pd_fw_info(struct device *dev, const struct firmware *fw,
