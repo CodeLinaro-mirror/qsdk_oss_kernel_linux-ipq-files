ebaa1f226ced52933034db80b567c804778e99ab
Timple Raj M <timple@qti.qualcomm.com>
2025-04-24 01:59:31 +0530

    drivers: clk: Add GCC driver support for IPQ5200
    This patch adds the GCC clock driver support for IPQ5200
    
    Change-Id: I390951f131ab51a068110c15f90e1a1f2ba9e9c1
    Signed-off-by: Timple Raj M <timple@qti.qualcomm.com>

---
+++ include/dt-bindings/clock/qcom,ipq5200-gcc.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2018,2020 The Linux Foundation. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#ifndef _DT_BINDINGS_CLOCK_IPQ_GCC_IPQ5200_H
+#define _DT_BINDINGS_CLOCK_IPQ_GCC_IPQ5200_H
+
+#define GCC_QUPV3_AHB_MST_CLK	0
+#define GCC_QUPV3_AHB_SLV_CLK	1
+#define GCC_QUPV3_UART1_CLK	2
+
+#endif
