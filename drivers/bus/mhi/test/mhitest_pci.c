6c4388fe7a9e4651efb8d343f32054bf88984ca2
Manikanta Mylavarapu <mmanikan@qti.qualcomm.com>
2026-06-02 22:02:52 +0530

    drivers: bus: mhi: test: fix soc_reset_delay_ms default value to 10ms
    Reduce the default soc_reset_delay_ms from 200 ms to 10 ms as higher
    delay, introduced during early QCN9625 bring-up, is no longer required.
    10 ms is sufficient and aligns with WiFi bring up (ATH) modules. Also
    update the reset polling logic in mhitest_q6_bcr_reset() to use 1 ms
    intervals, iterate directly based on soc_reset_delay_ms, return on
    successful transition to PBL, and validate completion based on device
    state instead of loop counter.
    
    Change-Id: I307a72de9cbc7c3c0d88a2120bb22a51953bc44c
    Signed-off-by: Manikanta Mylavarapu <mmanikan@qti.qualcomm.com>

--- drivers/bus/mhi/test/mhitest_pci.c
+++ drivers/bus/mhi/test/mhitest_pci.c
@@ -91,7 +91,7 @@ module_param(boot_debug_timeout, int, 0644);
 MODULE_PARM_DESC(boot_debug_timeout, "boot debug logs timeout in seconds");
 #define BOOT_DEBUG_TIMEOUT_MS		(boot_debug_timeout * 1000)
 
-int soc_reset_delay_ms = 200;
+int soc_reset_delay_ms = 10;
 module_param(soc_reset_delay_ms, int, 0644);
 MODULE_PARM_DESC(soc_reset_delay_ms, "soc reset delay in milliseconds");
 
@@ -1394,8 +1394,8 @@ out:
 
 void mhitest_q6_bcr_reset(struct mhitest_platform *mplat)
 {
-	int count = soc_reset_delay_ms / 100;
 	u32 device_ee = MHI_EE_MAX;
+	u32 count = 10;
 
 	pr_info("Issuing Q6 BCR reset Reset\n");
 
@@ -1403,17 +1403,17 @@ void mhitest_q6_bcr_reset(struct mhitest_platform *mplat)
 
 	pr_info("Q6 bcr reset issued\n");
 
-	while (count >= 0) {
+	while (count) {
 		device_ee = readl_relaxed(mplat->bar + QCN9625_BHI_EXECENV);
 		if (device_ee == MHI_EE_PBL) {
 			pr_info("Target switched to PBL, reset success, count: %d\n", count);
-			break;
+			return;
 		}
-		msleep(100);
+		mdelay(soc_reset_delay_ms);
 		count--;
 	}
 
-	if (count < 0)
+	if (device_ee != MHI_EE_PBL)
 		pr_info("Failed to switch to PBL after BCR reset\n");
 }
 
